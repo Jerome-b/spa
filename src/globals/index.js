2ca78be61313f2b21f8492f98230a34b9c67041e
Jerome-b <49626404+Jerome-b@users.noreply.github.com>
2022-04-24 23:06:02 -0700

    feat: add global folder to export assets from the index.js

---
+++ src/globals/index.js
@@ -0,0 +1,5 @@
+// export all assets in this global index.js:
+// prevent importing from assets folder in the different components of the app so that assets folder is only meant for storage
+
+export { PngIcons } from '../assets/icons'
+export { PngImages } from '../assets/images'
